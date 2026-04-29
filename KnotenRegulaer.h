9fa942d2b541a1418d3b7cb0be1df09de1b392d6
Luka Marinkic <luka.marinkic.personal@gmail.com>
2026-04-30 01:18:09 +0200

    Knoten.h hinzugefügt

---
+++ KnotenRegulaer.h
@@ -0,0 +1,43 @@
+// Da die KLasse Knoten von mehreren Datenstrkturen benötigt wird, tue ich sie mal fürs ganze Projekt schreiben.
+// Sie ist sehr nah an den Klassendiagram orientiert, das wir im Unterricht verwendet haben.
+
+#pragma once // Verhindert, dass die Klasse mehrfach inklusiert wird
+#include <memory>
+#include <utility>
+
+
+template<typename Typ>
+class Knoten{
+    public:
+    //Konstruktor
+    Knoten(Typ pInhalt){
+        aInhalt = &pInhalt;
+    };
+
+    //Destruktor
+    ~Knoten();
+
+    //Schutz gegen simples Kopieren, um zukünftige Fehler verzubeugen
+    Knoten(const Knoten&) = delete;
+    Knoten& operator=(const Knoten&) = delete;
+
+    // set und get Methoden:
+    void setzeNaechsten(std::unique_ptr<Knoten<Typ>> pNaechster){
+        aNaechster = std::move(pNaechster);
+    };
+    Knoten<Typ> gibNaechsten(){
+        return *aNaechster;
+    };
+
+    void setzeInhalt(Typ pInhalt){
+        aInhalt = std::move(pInhalt);
+    };
+    Typ gibInhalt(){
+        return *aInhalt;
+    };
+
+    private:
+    Typ aInhalt; //Daten, die in der Datenstruktur organisiert werden
+    std::unique_ptr<Knoten<Typ>> aNaechster; // Folge-Knoten
+
+}
