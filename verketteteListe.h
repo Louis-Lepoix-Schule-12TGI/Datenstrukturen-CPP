c2b9d99c4629424e71af5bb3b3a4b51858c6062c
Luka Marinkic <luka.marinkic.personal@gmail.com>
2026-05-04 21:43:02 +0200

    entfernen()-Methode erstellt

--- verketteteListe.h
+++ verketteteListe.h
@@ -21,27 +21,19 @@ class verketteteListe{
         return aAnfang.get() == nullptr;
     }
 
-    Typ entfernenVorne(){
-        if (istLeer()) {
-            throw std::logic_error("Fehler: Der Stapel ist leer. Es gibt nichts zum entfernen");
-        }
-        Typ rueckgabe = aAnfang->gibInhalt();
-
-        aAnfang = std::move(aAnfang->aNaechster);
-        
-        return rueckgabe;
-    }
-
     int anzahlElemente(){
         int anzahl = 0;
         std::unique_ptr<Knoten<Typ>> aktuellerZeiger = aAnfang;
+        Knoten<Typ> aktuellerKnoten = aktuellerZeiger->get();
         while(aktuellerZeiger.get() != nullptr){
+            aktuellerZeiger = aktuellerKnoten.gibNaechsten();
+            aktuellerKnoten = aktuellerZeiger->get();
             anzahl++;
         }
         return anzahl;
     }
 
-    Typ inhalt(int pIndex){
+    Typ inhalt(int pIndex) const{
         if(pIndex < 0){
             throw std::invalid_argument("Fehler: Der Index muss 0 oder hoeher sein!");
         }
@@ -49,7 +41,7 @@ class verketteteListe{
         if(istLeer())){
             throw std::logic_error("Fehler: Der Stapel ist leer. Es kann Nichts am Index existieren");
         }
-        Knoten<Typ> aktuellerKnoten = *aAnfang.get();
+        Knoten<Typ> aktuellerKnoten = aAnfang->get();
 
         for(int i = 0; i < pIndex; i++){ //For-loop endet, wenn aktuellerKnoten auf den Ziel-Knoten gesetz wurde 
             
@@ -64,7 +56,7 @@ class verketteteListe{
     
                     throw std::out_of_range(errorNachricht); //Error werfen
             }else{
-                aktuellerKnoten = *aktuellerKnoten.get() //aktuellen Knoten auf nächsten Knoten setzen
+                aktuellerKnoten.setzeNaechsten(aktuellerKnoten.gibNaechsten()); //aktuellen Knoten auf nächsten Knoten setzen
             }
         }
 
@@ -79,7 +71,7 @@ class verketteteListe{
         if(istLeer())){
             throw std::logic_error("Fehler: Der Stapel ist leer. Es kann Nichts am Index existieren");
         }
-        Knoten<Typ> aktuellerKnoten = *aAnfang.get();
+        Knoten<Typ> aktuellerKnoten = aAnfang->get();
 
         for(int i = 0; i < pIndex; i++){ //For-loop endet, wenn aktuellerKnoten auf den Ziel-Knoten gesetz wurde 
             
@@ -94,7 +86,7 @@ class verketteteListe{
     
                     throw std::out_of_range(errorNachricht); //Error werfen
             }else{
-                aktuellerKnoten = *aktuellerKnoten.get() //aktuellen Knoten auf nächsten Knoten setzen
+                aktuellerKnoten.setzeNaechsten(aktuellerKnoten.gibNaechsten()); //aktuellen Knoten auf nächsten Knoten setzen
             }
         }
         aktuellerKnoten.setzeInhalt(pInhalt);
@@ -105,9 +97,9 @@ class verketteteListe{
             throw std::invalid_argument("Fehler: Der Index muss 0 oder hoeher sein!");
         }
 
-        Knoten<Typ> aktuellerKnoten = *aAnfang.get();
+        Knoten<Typ> aktuellerKnoten = aAnfang->get();
         
-        //ACHTUNG: For-loop andes als bei ersetzen() und inhalt()
+        //ACHTUNG: For-loop anders als bei ersetzen() und inhalt()
         for(int i = 0; i < (pIndex-1); i++){ //For-loop endet, wenn aktuellerKnoten auf den Vorgänger des Ziel-Knoten gesetz wurde (fängt bei 0 erst gar nicht an)
             
             //Falls der Index zu groß ist
@@ -121,7 +113,7 @@ class verketteteListe{
     
                     throw std::out_of_range(errorNachricht); //Error werfen
             }else{
-                aktuellerKnoten = *aktuellerKnoten.get() //aktuellen Knoten auf nächsten Knoten setzen
+                aktuellerKnoten.setzeNaechsten(aktuellerKnoten.gibNaechsten()); //aktuellen Knoten auf nächsten Knoten setzen
             }
         }
 
@@ -138,7 +130,54 @@ class verketteteListe{
     void anhaengen(Typ pInhalt){
         einfuegen(anzahlElemente(); pInhalt);
     }
+
+    Typ entfernen(int pIndex){ //noch nicht fertig
+        if(pIndex < 0){
+            throw std::invalid_argument("Fehler: Der Index muss 0 oder hoeher sein!");
+        }
+
+        Knoten<Typ> aktuellerKnoten = aAnfang->get();
+        
+        //ACHTUNG: For-loop anders als bei ersetzen(), einfügen() und inhalt()
+        for(int i = 0; i < (pIndex-1); i++){ //For-loop endet, wenn aktuellerKnoten auf den Vorgänger des Ziel-Knoten gesetz wurde (fängt bei 0 erst gar nicht an)
+            
+            //Falls der Index zu groß ist
+            if(aktuellerKnoten.gibNaechsten().get() == nullptr && pIndex > (i){
+                //Error Nachricht schreiben
+                std::string errorNachricht = "Fehler: Es exestiert kein Elemnt beim gegebenen Index: " + 
+                                            static_cast<std::string>>(pIndex) + 
+                                            " .  Hoehster Index:" +
+                                            static_cast<std::string>(anzahlElemente()-1);
     
+                    throw std::out_of_range(errorNachricht); //Error werfen
+            }else{
+                aktuellerKnoten.setzeNaechsten(aktuellerKnoten.gibNaechsten()); //aktuellen Knoten auf nächsten Knoten setzen
+            }
+        }
+
+        Knoten<Typ> rueckgabeKnoten = aktuellerKnoten.gibNaechsten()->get();
+
+        if(rueckgabeKnoten.get() != nullptr){ // Falls ein Nachfolger existiert
+            aktuellerKnoten.setzeNaechsten(rueckgabeKnoten.gibNaechsten()); //setze aktuellerKnoten.aNaechster auf rueckgabeKnoten.aNaechster
+        }else{
+            std::unique_ptr<Knoten<Typ>> tmpZeiger = nullptr;
+            aktuellerKnoten.setzeNaechsten(tmpZeiger);
+        }
+
+        return rueckgabeKnoten.gibInhalt();
+    };
+
+    Typ entfernenVorne(){
+        if (istLeer()) {
+            throw std::logic_error("Fehler: Der Stapel ist leer. Es gibt nichts zum entfernen");
+        }
+        Typ rueckgabe = aAnfang->gibInhalt();
+
+        aAnfang = std::move(aAnfang->aNaechster);
+        
+        return rueckgabe;
+    }
+
 
     
     private:
